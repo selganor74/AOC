970f1cd190411e665b95f4c753794f009bf231d3
selganor <selganor74@gmail.com>
2024-12-16 00:05:33 +0100

    day 5 completed

--- 2024/day05/Program.cs
+++ 2024/day05/Program.cs
@@ -1,4 +1,6 @@
-﻿var lines = File.ReadAllLines("input.txt");
+﻿using System.Net.Http.Headers;
+
+var lines = File.ReadAllLines("input.txt");
 
 static bool IsOrdered(List<int[]> rules, int[] update)
 {
@@ -33,5 +35,32 @@ var rules = pageOrderingRulesSection.Select(x => x.Split('|').Select(int.Parse).
 var updates = pagesForUpdateSection.Select(x => x.Split(',').Select(int.Parse).ToArray()).ToList();
 
 
-var part1 = updates.Where(u => IsOrdered(rules, u)).Select(u => u[u.Length/2]).Sum();
+var part1 = updates.Where(u => IsOrdered(rules, u)).Select(u => u[u.Length / 2]).Sum();
 Console.WriteLine($"Part1: {part1}");
+
+var incorrectlyOrdered = updates.Where(u => !IsOrdered(rules, u)).ToList();
+var part2 = incorrectlyOrdered.Select(update =>
+        {
+            retry:
+            for (int i = 0; i < update.Length - 1; i++)
+            {
+                for (int v = i + 1; v < update.Length; v++)
+                {
+                    var rule = rules.FirstOrDefault(r => r[0] == update[i] && r[1] == update[v]);
+                    if (rule is null)
+                    {
+                        // this element is not in the right order so we swap 
+                        var temp = update[i];
+                        update[i] = update[v];
+                        update[v] = temp;
+                        goto retry; 
+                    }
+                }
+            }
+            // when we don't fall in the goto, the thing is ordered
+            return update;
+
+        })
+    .Select(u => u[u.Length / 2]).Sum();
+
+Console.WriteLine($"Part2: {part2}");
