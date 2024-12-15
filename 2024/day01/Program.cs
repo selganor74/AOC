d1463fe5b3c956b6b043ca653ab1c942fd35a5bd
selganor <selganor74@gmail.com>
2024-12-15 18:16:59 +0100

    day1 part 1 done

---
+++ 2024/day01/Program.cs
@@ -0,0 +1,16 @@
+﻿// Run this program with 
+//    dotnet run
+var lines = File.ReadAllLines("input.txt");
+var values = lines.Select(x => 
+    x.Split(' ')
+        .Where(r => !string.IsNullOrWhiteSpace(r))
+        .Select(n => int.Parse(n))
+);
+
+var listA = values.Select(x => x.First()).OrderBy(x=>x).ToList();
+var listB = values.Select(x => x.Last()).OrderBy(x=>x).ToList();
+
+var part1 = listA.Zip(listB).Select(t => Math.Abs(t.Second - t.First)).Sum();
+Console.WriteLine($"Part1: {part1}");
+
+
