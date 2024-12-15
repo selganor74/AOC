c959905b644301bd7f4f38beb52017fd47bc3bf6
selganor <selganor74@gmail.com>
2024-12-15 18:37:10 +0100

    day 1 completed

--- 2024/day01/Program.cs
+++ 2024/day01/Program.cs
@@ -13,4 +13,14 @@ var listB = values.Select(x => x.Last()).OrderBy(x=>x).ToList();
 var part1 = listA.Zip(listB).Select(t => Math.Abs(t.Second - t.First)).Sum();
 Console.WriteLine($"Part1: {part1}");
 
+var listBGrouped = listB.GroupBy(x => x); // groups the list B by value
+var part2 = listA.Distinct() // take the distinct values of list A
+                 .Select(current => listBGrouped.Where(x => x.Key == current)     // takes the listB's group corresponding to current value of A
+                                                .Select(x => current * x.Count()) // multiplies the current value of listA by number of occurrences in listB 
+                                                .DefaultIfEmpty()                 // zero if no group exists
+                                                .Single()                         // takes the single value produced
+                 )
+                 .Sum();    // adds up all the values
+Console.WriteLine($"Part2: {part2}");
+
 
