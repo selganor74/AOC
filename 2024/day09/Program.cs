743d3b9cc1bc956d5f092fb4a709e4629b4738f0
selganor <selganor74@gmail.com>
2024-12-19 20:39:52 +0100

    day 9 completed

--- 2024/day09/Program.cs
+++ 2024/day09/Program.cs
@@ -1,53 +1,156 @@
 ﻿var data = File.ReadAllLines("input.txt");
 
 var diskContent = data[0];
-List<DiskPosition> diskLayout = new();
 
-var id = 0;
-var position = 0;
-for (int i = 0; i < diskContent.Length; i += 2)
-{
-    var fileLen = int.Parse("" + diskContent[i]);
-
-    for (int fi = 0; fi < fileLen; fi++)
-        diskLayout.Add(new OccupiedByFile(position++, id));
-
-    id++;
-
-    if (i + 1 >= diskContent.Length) continue;
-
-    var wastedSpace = int.Parse("" + diskContent[i + 1]);
-    for (int wi = 0; wi < wastedSpace; wi++)
-        diskLayout.Add(new WastedSpace(position++));
-}
-diskLayout.Dump();
-
-var done = false;
-do
-{
-    var source = diskLayout.Where(t => t is OccupiedByFile).Select(t => t as OccupiedByFile).OrderBy(t => t!.Position).Last()!;
-    var dest = diskLayout.Where(t => t is WastedSpace).Select(t => t as WastedSpace).OrderBy(t => t!.Position).First()!;
-    if (source.Position < dest.Position)
-        done = true;
-    else
-    {
-        diskLayout[dest.Position] = dest.ReplaceWith(source);
-        diskLayout[source.Position] = source.Replace;
-    }
-} while (!done);
-diskLayout.Dump();
-
-var part1 = diskLayout.CalculatePart1();
+var diskLayout = BuildDiskLayout(diskContent);
+PackForPart1(diskLayout);
+// diskLayout.Dump();
+var part1 = diskLayout.CalculateChecksum();
 Console.WriteLine("Part1: " + part1);
+
+diskLayout = BuildDiskLayout(diskContent);
+PackForPart2(diskLayout);
+// diskLayout.Dump();
+var part2 = diskLayout.CalculateChecksum();
+Console.WriteLine("Part2: " + part2);
+
+void PackForPart2(List<DiskPosition> diskLayout)
+{
+    var lastId = diskLayout
+            .Where(t => t is OccupiedByFile)
+            .Select(t => t as OccupiedByFile)
+            .OrderBy(t => t!.Id)
+            .Last()!.Id;
+    for (var id = lastId; id >= 0; id--)
+    {
+        var fileLen = diskLayout
+            .Where(t => t is OccupiedByFile)
+            .Select(t => t as OccupiedByFile)
+            .Where(t => t!.Id == id)
+            .Count();
+
+        var fileFirstPos = diskLayout
+            .Where(t => t is OccupiedByFile)
+            .Select(t => t as OccupiedByFile)
+            .Where(t => t!.Id == id)
+            .Min(t => t!.Position);
+
+        // Console.WriteLine($"FileId: {id} Length: {fileLen}");
+
+        // var isFirst = true;
+        // var wasteId = 0;
+        // foreach (var dp in diskLayout.ToList())
+        // {
+        //     if (dp is OccupiedByFile)
+        //     {
+        //         isFirst = true;
+        //         continue;
+        //     }
+
+        //     if (isFirst)
+        //     {
+        //         wasteId++;
+        //         isFirst = false;
+        //     }
+
+        //     diskLayout[dp.Position] = (dp as WastedSpace)! with { WasteId = wasteId };
+        // }
+
+
+        var freeSpace = diskLayout
+            .Where(t => t is WastedSpace)
+            .Select(t => t as WastedSpace)
+            .Where(t => t!.Position < fileFirstPos)
+            .GroupBy(t => t!.WasteId)
+            .Where(g => g.Count() >= fileLen)
+            .OrderBy(g => g.Min(e => e!.Position))
+            .FirstOrDefault()?
+            .FirstOrDefault()?
+            .WasteId;
+
+        if (freeSpace is null) continue;
+
+        var sourceFilePositions = diskLayout
+            .Where(d => d is OccupiedByFile && ((OccupiedByFile)d).Id == id)
+            .Select(d => (OccupiedByFile)d)
+            .ToList();
+
+        foreach (var source in sourceFilePositions)
+        {
+            var dest = diskLayout
+                .Where(t => t is WastedSpace space && space.WasteId == freeSpace)
+                .Select(t => t as WastedSpace)
+                .OrderBy(t => t!.Position)
+                .First()!;
+
+            diskLayout[dest.Position] = dest.ReplaceWith(source);
+            diskLayout[source.Position] = source.ReplaceWith(dest);
+
+        }
+    }
+}
+
+List<DiskPosition> BuildDiskLayout(string diskContent)
+{
+
+    List<DiskPosition> diskLayout = new();
+
+    var id = 0;
+    var position = 0;
+    for (int i = 0; i < diskContent.Length; i += 2)
+    {
+        var fileLen = int.Parse("" + diskContent[i]);
+
+        for (int fi = 0; fi < fileLen; fi++)
+            diskLayout.Add(new OccupiedByFile(position++, id));
+
+        id++;
+
+        if (i + 1 >= diskContent.Length) continue;
+
+        var wastedSpace = int.Parse("" + diskContent[i + 1]);
+        for (int wi = 0; wi < wastedSpace; wi++)
+            diskLayout.Add(new WastedSpace(position++, id));
+    }
+    return diskLayout;
+}
+
+void PackForPart1(List<DiskPosition> diskLayout)
+{
+    var done = false;
+    do
+    {
+        var source = diskLayout
+            .Where(t => t is OccupiedByFile)
+            .Select(t => t as OccupiedByFile)
+            .OrderBy(t => t!.Position)
+            .Last()!;
+
+        var dest = diskLayout
+            .Where(t => t is WastedSpace)
+            .Select(t => t as WastedSpace)
+            .OrderBy(t => t!.Position)
+            .First()!;
+
+        if (source.Position < dest.Position)
+            done = true;
+        else
+        {
+            diskLayout[dest.Position] = dest.ReplaceWith(source);
+            diskLayout[source.Position] = source.ReplaceWith(dest);
+        }
+    } while (!done);
+}
 
 
 public abstract record DiskPosition(int Position);
+
 public record OccupiedByFile(int Position, long Id) : DiskPosition(Position)
 {
-    public WastedSpace Replace => new(Position);
+    public WastedSpace ReplaceWith(WastedSpace waste) => new(Position, waste.WasteId);
 }
 
-public record WastedSpace(int Position) : DiskPosition(Position)
+public record WastedSpace(int Position, int WasteId) : DiskPosition(Position)
 {
     public OccupiedByFile ReplaceWith(OccupiedByFile file) => new(Position, file.Id);
 }
@@ -59,7 +162,7 @@ public static class DiskLayoutContent
         foreach (var p in dl) Console.WriteLine(p);
     }
 
-    public static long CalculatePart1(this IEnumerable<DiskPosition> dl)
+    public static long CalculateChecksum(this IEnumerable<DiskPosition> dl)
     {
         return dl.Select(t => t as OccupiedByFile).Where(t => t is not null).Sum(t => t!.Id * t!.Position);
     }
