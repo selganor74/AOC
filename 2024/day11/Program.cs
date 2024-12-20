f35721653082ff3a293ccbc7c58c75a61ef2f0ac
selganor <selganor74@gmail.com>
2024-12-20 21:44:00 +0100

    day11 part 1 done

---
+++ 2024/day11/Program.cs
@@ -0,0 +1,50 @@
+﻿var stones = File.ReadAllLines("input.txt")[0].Split(' ').Select(long.Parse).ToList();
+
+List<long> newArrangement = GetStoneArrangementAfterNBlinks(ref stones, 25);
+
+var part1 = newArrangement.Count();
+Console.WriteLine($"Part1: {part1}");
+
+bool HasEvenDigits(long stone)
+{
+    return 0 == (stone.ToString().Length % 2);
+}
+
+IEnumerable<long> SplitStone(long stone)
+{
+    var asString = stone.ToString();
+    var first = asString.Substring(0, asString.Length / 2);
+    var second = asString.Substring(asString.Length / 2, asString.Length / 2);
+
+    return [long.Parse(first), long.Parse(second)];
+}
+
+List<long> GetStoneArrangementAfterNBlinks(ref List<long> stones, int totalBlinks)
+{
+    List<long> newArrangement = [];
+    for (var blink = 1; blink <= totalBlinks; blink++)
+    {
+        newArrangement = [];
+
+        foreach (var stone in stones)
+        {
+            if (stone == 0)
+            {
+                newArrangement.Add(1);
+                continue;
+            }
+
+            if (HasEvenDigits(stone))
+            {
+                newArrangement.AddRange(SplitStone(stone));
+                continue;
+            }
+
+            newArrangement.Add(stone * 2024);
+        }
+
+        stones = newArrangement;
+    }
+
+    return newArrangement;
+}
